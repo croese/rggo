0d8564c5fbb7d500d438044f56c5bae710505350
Christian Roese <croese@8thlight.com>
2024-09-27 09:14:23 -0400

    update wc to read from multiple files

--- firstProgram/wc/main.go
+++ firstProgram/wc/main.go
@@ -13,7 +13,26 @@ func main() {
 	bytes := flag.Bool("b", false, "Count bytes")
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines, *bytes))
+	total := 0
+	if len(flag.Args()) == 0 {
+		total = count(os.Stdin, *lines, *bytes)
+	} else {
+		for _, fname := range flag.Args() {
+			f, err := os.Open(fname)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			total += count(f, *lines, *bytes)
+
+			if err = f.Close(); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
+	}
+
+	fmt.Println(total)
 }
 
 func count(r io.Reader, countLines bool, countBytes bool) int {
